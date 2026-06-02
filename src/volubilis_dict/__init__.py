e70a80708331b23d94f1633aaddd771cb12f5869
Thomas Podman Barth <windwerfer@gmail.com>
2026-06-02 07:42:15 +0000

    moved version nr into project.toml

--- src/volubilis_dict/__init__.py
+++ src/volubilis_dict/__init__.py
@@ -1,3 +1,9 @@
 """Volubilis dictionary processor package."""
 
-__version__ = "1.1"
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("volubilis-dict")
+except PackageNotFoundError:
+    __version__ = "0.0.0.dev0"  # fallback when running from source without install
+
